7f0c5b5040577488d206d943d5bd4e686b85818e
Ran Kirlian <rankirlian@gmail.com>
2021-11-01 09:49:05 +0100

    Calendar-App first commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/login" exact component={LoginScreen} />
+                    <Route path="/" exact component={CalendarScreen} />
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
